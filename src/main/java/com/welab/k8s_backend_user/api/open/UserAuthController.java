0c509d162d280ace1b47dad722a05552ce673e6d
NamSangwon <127469500+NamSangwon@users.noreply.github.com>
2025-06-17 14:14:19 +0900

    blue green 2

--- src/main/java/com/welab/k8s_backend_user/api/open/UserAuthController.java
+++ src/main/java/com/welab/k8s_backend_user/api/open/UserAuthController.java
@@ -32,6 +32,6 @@ public class UserAuthController {
 
     @GetMapping(value = "/test")
     public ApiResponseDto<String> test() {
-        return ApiResponseDto.createOK("버전 1입니다.");
+        return ApiResponseDto.createOK("버전 2입니다.");
     }
 }
